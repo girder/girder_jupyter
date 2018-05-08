3b0b78d8dd0bc2540a895b62d4a59bda5e092308
Chris Harris <chris.harris@kitware.com>
2018-05-08 13:28:24 -0400

    Remove trailing whitespace

--- tests/test_girder_contents_api.py
+++ tests/test_girder_contents_api.py
@@ -12,7 +12,7 @@ import json
 from .constants import GIRDER_API_KEY, GIRDER_API_URL
 
 class GirderContentsTest(APITest):
-    gc = girder_client.GirderClient(apiUrl=GIRDER_API_URL)  
+    gc = girder_client.GirderClient(apiUrl=GIRDER_API_URL)
 
     def _get_girder_path(self, path):
         return ('/user/%s/Private/%s' % (self.user['login'],
@@ -27,7 +27,7 @@ class GirderContentsTest(APITest):
         cls.gc.authenticate(apiKey=GIRDER_API_KEY)
         cls.user = cls.gc.get("user/me")
         super(APITest, cls).setup_class()
-        
+
 
     def delete_dir(self, path):
         path = self._get_girder_path(path)
@@ -39,7 +39,7 @@ class GirderContentsTest(APITest):
         resource = self._resource(path)
         self.gc.delete("item/{}".format(resource['_id']))
 
-    # def setUp(self):        
+    # def setUp(self):
     #     for d in (self.dirs + self.hidden_dirs):
     #         self.make_dir(d)
     #         self.addCleanup(partial(self.delete_dir, d))
@@ -91,7 +91,7 @@ class GirderContentsTest(APITest):
         return resource['_modelType'] in model_type
 
     def _resource(self, path):
-        
+
         return self.gc.resourceLookup(path, test=True)
 
 
@@ -112,7 +112,7 @@ class GirderContentsTest(APITest):
                                             parentType=grandpa['_modelType'])
         elif  parent['_modelType'] not in ['user', 'folder']:
             self.fail('Permission denied: %s' % parent_path)
-        
+
         return parent
 
     def _get_or_create_file_parent(self, path):
@@ -130,7 +130,7 @@ class GirderContentsTest(APITest):
             self.fail('Only an item can contain a file')
         elif resource['_modelType'] == 'folder':
             self.fail('Only an item can contain a file')
-        
+
 
 
     def make_dir(self, path):
@@ -151,7 +151,7 @@ class GirderContentsTest(APITest):
             self.fail('The requested resource is not a folder %s' % path)
         else:
             return resource
-        
+
 
     def make_txt(self, path, content):
         parent = self._get_or_create_file_parent(path)
@@ -167,17 +167,15 @@ class GirderContentsTest(APITest):
         stream = BytesIO(content)
         self.gc.uploadFile(parent['_id'], stream, os.path.basename(path), size)
 
-    
+
     def make_nb(self, path, nb):
         """Make a notebook file at a given api_path"""
         content = json.dumps(nb, indent=2)
         self.make_txt(path, content)
         # os_path = self.to_os_path(api_path)
 
-        
+
         # with io.open(os_path, 'w', encoding='utf-8') as f:
         #     write(nb, f, version=4)
 
     # def test_make_txttt(self)
-        
-
